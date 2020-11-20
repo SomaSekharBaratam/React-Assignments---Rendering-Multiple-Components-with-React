8473ab10f917b37d45a4b68f457a8e9b924aa8e2
SomaSekharBaratam <68215926+SomaSekharBaratam@users.noreply.github.com>
2020-11-20 20:01:29 +0530

    created tag with attribute data-na-test

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+<p data-ns-test="project-name">Laboratory System</p>
+<p data-ns-test="project-description">Laboratory System with management</p>
+
+    </div>
   )
 }
 
